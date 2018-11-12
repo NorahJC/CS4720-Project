f45843c0d836467dffa2bee879eb8a7c137258b7
drennanana <alexdrennan@gmail.com>
2018-11-12 11:29:22 -0500

    fixing dashboard

--- man_dashboard.php
+++ man_dashboard.php
@@ -10,10 +10,10 @@
 		$userinfo = $dbMan->getTeacher($_SESSION['uname']);
 	}
 	$classList = $dbMan->getUsersClasses($userinfo[1]);	
-
+	
+	unset($_SESSION['currentClassID']);	//removes a session variable set from homepage
 ?>
-
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>M.A.N. High School Portal Dashboard</title>
@@ -44,8 +44,7 @@
   </style>  
 </head>
 <body>
-
-<nav class="navbar navbar-inverse">
+ <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -54,16 +53,19 @@
         <span class="icon-bar"></span>                        
       </button>
       <a class="navbar-brand" href="#"><img src="logo_black.jpg" alt="homepage logo" class="homepage_logo" width = "90%" height = "80px" ></a>
-	  <a href="#" style = "color: white; line-height:450%; font-size:20px; padding: 30px">Welcome, <?php echo $userinfo[0]?></a>
+	  <a href="#" style = "color: white; font-size:20px; padding: 30px">Welcome, <?php echo $userinfo[0]?></a>
 	</div>
 </nav>	
 <div class = "container-fluid-1">
-
-		
+ 		
 		<?php 
 		
-		
-			$count = 0;	//after 3 move to next row
+			if($classList == -1){
+				$classList = array();
+				echo "<h4><b>You are not associated with any classes.</b></h4>";
+				echo "<a href='login.php'>Go back to Main Page</a>";
+			}
+			
 			foreach($classList as $class){
 				$info = $dbMan->getClassInfo($class);
 				echo '<form class="dashboard"  action="man_homepage.php" method="post">';
